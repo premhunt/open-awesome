baa450c2e0c2336583d9e30f648da4a8c65c33bd
Ethan Hann <ethanhann@gmail.com>
2014-05-01 22:21:27 -0400

    Convert system controller to audit controller

---
+++ src/Ehann/Bundle/OpenAwesomeBundle/Controller/AuditController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Ehann\Bundle\OpenAwesomeBundle\Controller;
+
+use Ehann\Bundle\WebServiceBundle\Response\WebServiceResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class AuditController extends Controller
+{
+    public $esParams;
+
+    public function __construct()
+    {
+        $this->esParams = array(
+            'index' => 'open-awesome'
+        );
+    }
+
+    /**
+     * @return \Elasticsearch\Client
+     */
+    function getElasticsearch()
+    {
+        return $this->get('ehann_open_awesome.elasticsearch_client');
+    }
+
+    /**
+     * @param Request $request
+     * @return WebServiceResponse
+     * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
+     */
+    public function indexAction(Request $request)
+    {
+        if (!$request->query->has('q')) {
+            throw new BadRequestHttpException('"q" parameter is required');
+        }
+        $this->esParams['type'] = $request->query->has('component') ? $request->query->get('component') : '';
+        $this->esParams['body']['query']['queryString']['query'] = $request->query->get('q');
+        $queryResponse = $this->getElasticsearch()->search($this->esParams);
+        return new WebServiceResponse($queryResponse);
+    }
+
+    /**
+     * @param $component
+     * @param $id
+     * @return WebServiceResponse
+     */
+    public function getAction($component, $id)
+    {
+        $this->esParams['id'] = $id;
+        $this->esParams['type'] = $component;
+        $document = $this->getElasticsearch()->get($this->esParams);
+        return new WebServiceResponse($document);
+    }
+}
