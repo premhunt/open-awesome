77d6171be8c9ace30af4a09bf5edf15f82f1d627
Ethan Hann <ethanhann@gmail.com>
2014-05-03 11:42:33 -0400

    Get query term from route in controller

--- webapp/app/js/controllers.js
+++ webapp/app/js/controllers.js
@@ -3,14 +3,12 @@
 /* Controllers */
 
 angular.module('openAwesome.controllers', []);
-angular.module('openAwesome.controllers').controller('MainCtrl', function ($scope, $resource, $modal, $log) {
-    $scope.searchQuery = '';
+angular.module('openAwesome.controllers').controller('MainCtrl', function ($scope, $resource, $routeParams) {
+    $scope.searchQuery = angular.isDefined($routeParams.q) ? $routeParams.q : '';
     $scope.search = function () {
-        $scope.searchResults = $resource('/api/app_dev.php/audit').get({ q: $scope.searchQuery });
+        if (angular.isDefined($scope.searchQuery) && $scope.searchQuery !== '') {
+            $scope.searchResults = $resource('/api/app_dev.php/audit').get({ q: $scope.searchQuery });
+        }
     };
-    $scope.isArray = angular.isArray;
-    $scope.isString = angular.isString;
-    $scope.humanize = function (text) {
-        return angular.isString(text) ? text.replace(/(_)/g, ' ') : text;
-    };
+    $scope.search();
 });
