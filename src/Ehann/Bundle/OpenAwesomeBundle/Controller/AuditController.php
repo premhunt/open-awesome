732d72387c91784596ed2a7b9ef4d026eae87eec
Ethan Hann <ethanhann@gmail.com>
2014-05-04 00:55:12 -0400

    Persist additional components

--- src/Ehann/Bundle/OpenAwesomeBundle/Controller/AuditController.php
+++ src/Ehann/Bundle/OpenAwesomeBundle/Controller/AuditController.php
@@ -86,8 +86,90 @@ class AuditController extends Controller
         // BIOS
         $this->persistComponent($manager, $xmlContent->bios, 'SysHwBios', 'bios_', $system);
         // SCSI Controllers
-        foreach ($xmlContent->scsi_controllers->children() as $scsiControllerXml) {
-            $this->persistComponent($manager, $scsiControllerXml, 'SysHwScsiController', 'scsi_controller_', $system);
+        foreach ($xmlContent->scsi_controllers->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwScsiController', 'scsi_controller_', $system);
+        }
+        // Processor
+        $this->persistComponent($manager, $xmlContent->processor, 'SysHwProcessor', 'processor_', $system);
+        // Memory
+        foreach ($xmlContent->memory->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwMemory', '', $system);
+        }
+        // Motherboard
+        $this->persistComponent($manager, $xmlContent->motherboard, 'SysHwMotherboard', '', $system);
+        // Optical Drives
+        foreach ($xmlContent->optical_drives->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwOpticalDrive', 'optical_drive_', $system);
+        }
+        // Video Cards
+        foreach ($xmlContent->video_cards->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwVideo', 'video_', $system);
+        }
+        // Monitors
+        foreach ($xmlContent->sound_cards->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwMonitor', '', $system);
+        }
+        // Sound Cards
+        foreach ($xmlContent->sound_cards->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwSound', 'sound_', $system);
+        }
+        // Hard Drives
+        foreach ($xmlContent->hard_disks->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwHardDrive', 'hard_drive_', $system);
+        }
+        // Partitions
+        foreach ($xmlContent->partitions->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwPartition', 'partition_', $system);
+        }
+        // Shares
+        foreach ($xmlContent->shares->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysSwShare', 'share_', $system);
+        }
+        // Network Cards
+        foreach ($xmlContent->network_cards->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwNetworkCard', 'net_', $system);
+        }
+        // Addresses
+        foreach ($xmlContent->addresses->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysHwNetworkCardIp', '', $system);
+        }
+        // DNS
+        foreach ($xmlContent->dns->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysSwDns', 'dns_', $system);
+        }
+        // Printers
+//        foreach ($xmlContent->printers->children() as $componentXml) {
+//            $this->persistComponent($manager, $componentXml, 'System', 'printer_');
+//        }
+        // Logs
+        foreach ($xmlContent->logs->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysSwLog', 'log_', $system);
+        }
+        // Pagefiles
+        foreach ($xmlContent->pagefiles->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysSwPagefile', '', $system);
+        }
+        // Users
+        foreach ($xmlContent->users->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysSwUser', 'user_', $system);
+        }
+        // Software
+        foreach ($xmlContent->software->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysSwSoftware', 'software_', $system);
+        }
+        // Services
+        foreach ($xmlContent->services->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysSwService', 'service_', $system);
+        }
+        // Database
+        $this->persistComponent($manager, $xmlContent->database, 'SysSwDatabase', 'db_', $system);
+        // Software Keys
+        foreach ($xmlContent->software_keys->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysSwSoftwareKey', 'key_', $system);
+        }
+        // Routes
+        foreach ($xmlContent->routes->children() as $componentXml) {
+            $this->persistComponent($manager, $componentXml, 'SysSwRoute', '', $system);
         }
 
         $manager->flush();
@@ -102,12 +184,13 @@ class AuditController extends Controller
             $newNodeName = str_replace($componentPrefix, '', $node->getName());
             $componentXml->addChild($newNodeName)->{0} = (string)$node;
         }
-        $deserializationClass = 'Ehann\Bundle\OpenAwesomeBundle\Entity\\' . $componentClass;
-        $component = $this->get('jms_serializer')->deserialize($componentXml->asXml(), $deserializationClass, 'xml');
+        $class = 'Ehann\Bundle\OpenAwesomeBundle\Entity\\' . $componentClass;
+        $component = $this->get('jms_serializer')->deserialize($componentXml->asXml(), $class, 'xml');
         if ($system) {
             $component->setSystem($system);
         }
         $component->setTimestamp($system->getTimestamp());
+
         $existingComponent = $manager->getRepository('EhannOpenAwesomeBundle:' . $componentClass)->findOneBy(array('system' => $system->getId()));
         if ($existingComponent) {
             $component->setId($existingComponent->getId());
