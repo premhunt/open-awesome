73acb1cf0f1b9a4c1cab63a36f5873246076e1be
Ethan Hann <ethanhann@gmail.com>
2014-05-04 14:16:33 -0400

    Show hits in groups

--- webapp/app/js/controllers.js
+++ webapp/app/js/controllers.js
@@ -7,7 +7,20 @@ angular.module('openAwesome.controllers').controller('MainCtrl', function ($scop
     $scope.searchQuery = angular.isDefined($routeParams.q) ? $routeParams.q : '';
     $scope.search = function () {
         if (angular.isDefined($scope.searchQuery) && $scope.searchQuery !== '') {
-            $scope.searchResults = $resource('/api/app_dev.php/audit').get({ q: $scope.searchQuery });
+            $scope.searchResults = $resource('/api/app_dev.php/audit').get({ q: $scope.searchQuery }, function (data) {
+                $scope.groupedHits = [];
+                var hits = data.hits.hits;
+                var group = [];
+                for (var i = 0; i < hits.length; i++) {
+                    group.push(hits[i]);
+                    if (i % 2 !== 0) {
+                        $scope.groupedHits.push(group);
+                        group = [];
+                    } else if (i === hits.length - 1) {
+                        $scope.groupedHits.push(group);
+                    }
+                }
+            });
         }
     };
     $scope.search();
